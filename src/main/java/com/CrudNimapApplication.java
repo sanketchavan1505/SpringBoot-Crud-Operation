250a906fad4a674059828f1e14ea6f9f7c20c284
sanketchavan1505 <sanketchavan1505@gmail.com>
2024-08-03 15:42:40 +0530

    SpringBoot

---
+++ src/main/java/com/CrudNimapApplication.java
@@ -0,0 +1,13 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrudNimapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CrudNimapApplication.class, args);
+	}
+
+}
